9d9c02019d270307dc1698246bdec0d5eeb9557e
Nicolás Mato <matonicolas@gmail.com>
2024-01-05 02:59:36 -0300

    Create form.php

---
+++ form.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+
+    // Reemplaza "tucorreo@example.com" con tu dirección de correo electrónico
+    $destinatario = "tucorreo@example.com";
+    $asunto = "Nuevo mensaje de contacto";
+
+    $contenido = "Nombre: $nombre\n";
+    $contenido .= "Correo Electrónico: $email\n";
+    $contenido .= "Mensaje:\n$mensaje";
+
+    // Enviar correo electrónico
+    mail($destinatario, $asunto, $contenido);
+
+    // Redirigir al usuario después de enviar el formulario
+    header("Location: gracias.html");
+} else {
+    // Si alguien intenta acceder directamente al script PHP
+    header("Location: index.html");
+}
+?>
