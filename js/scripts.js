21e3fc0226f96d88cb5736f2470d1f35eb02d0ea
Nicolás Mato <matonicolas@gmail.com>
2025-03-23 03:27:22 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -35,4 +35,28 @@ let isVisible = false;
 setInterval(() => {
     cursor.style.visibility = isVisible ? 'visible' : 'hidden';
     isVisible = !isVisible;
-}, 300); // velocidad en ms de parpadeo
+}, 400); // velocidad en ms de parpadeo
+
+// Obtener la fecha y hora actual en la zona horaria de Argentina (GMT-3) y formatearla
+const now = new Date();
+const options = {
+    weekday: 'long', 
+    month: 'long', 
+    day: 'numeric', 
+    hour: '2-digit', 
+    minute: '2-digit', 
+    second: '2-digit',
+    timeZone: 'America/Argentina/Buenos_Aires', 
+    year: 'numeric'
+};
+
+// Formatear la fecha y hora con los segundos incluidos
+let formattedDate = now.toLocaleString('en-US', options);
+
+// Reemplazar las comas por espacios y ajustar el formato
+formattedDate = formattedDate.replace(/,/g, '');
+
+// Colocar la información en el HTML cuando el DOM esté listo
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("sysInfo").innerHTML = `System information as of ${formattedDate}`;
+});
